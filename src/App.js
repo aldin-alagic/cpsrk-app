895494d473963be863b2d40138224be6b3c78f2a
aldin-alagic <aldin@dysordian.com>
2021-04-23 01:41:55 +0200

    Add ScrollToTop component to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from "react";
 import { Route, Redirect } from "react-router-dom";
 
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 
+import Event from "./pages/Event/Event";
 import Home from "./pages/Home/Home";
 
 //Static
 import "./App.scss";
 
 class App extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0)
+  }
   render() {
     return (
-      <>
+      <ScrollToTop>
         <Header />
         <Main>
+        <Route path="/event" component={Event} />
           <Route path="/" component={Home} />
           <Redirect to="/" />
         </Main>
         <Footer />
-      </>
+      </ScrollToTop>
     );
   }
 }
