81f8ead4c0f165b547ca22c29800e26a5ff19aa3
aldin-alagic <aldin@dysordian.com>
2021-04-23 03:59:26 +0200

    Add Event route and page to App component

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 
-import Home from "./pages/Home/Home";
 import Events from "./pages/Events/Events";
+import Event from "./pages/Event/Event";
+import Home from "./pages/Home/Home";
+
 
 //Static
 import "./App.scss";
@@ -24,6 +26,7 @@ class App extends Component {
         <Main>
           <Switch>
             <Route path="/events" component={Events} />
+            <Route path="/event" component={Event} />
             <Route path="/" component={Home} />
             <Redirect to="/" />
           </Switch>
