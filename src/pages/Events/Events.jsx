6907a6d6baeb72345c66fb0af841d0629ed8d99a
aldin-alagic <aldin@dysordian.com>
2021-04-23 02:48:17 +0200

    Create Events page that displays events using EventInfoCard component

---
+++ src/pages/Events/Events.jsx
@@ -0,0 +1,35 @@
+import Section from "../../components/Section/Section";
+import Grid from "../../components/Grid/Grid";
+import EventInfoCard from './../../components/EventInfoCard/EventInfoCard';
+
+import "./Events.scss";
+import * as data from "../../utils/data"
+
+function Events() {
+    //This events mock data will be replaced with an actual Axios or ApiSauce API call
+    const events = data.mockEvents;
+
+  return (
+    <Section title="Events">
+      <Grid columns="4">
+      {events.length ? (
+            events.map((eventItem) => (
+              <EventInfoCard
+                key={eventItem.id.toString()}
+                title={eventItem.title}
+                location={eventItem.location}
+                date={eventItem.date}
+                attendance={eventItem.attendance}
+                maxAttendance={eventItem.maxAttendance}
+                company={eventItem.company}
+              />
+            ))
+          ) : (
+            <p>There are no events!</p>
+          )}
+      </Grid>
+    </Section>
+  );
+}
+
+export default Events;
