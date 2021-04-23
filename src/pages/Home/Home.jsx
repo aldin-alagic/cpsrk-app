f1810d1037c57d55176ee9d11fed9548e1d6e94e
aldin-alagic <aldin@dysordian.com>
2021-04-23 02:46:10 +0200

    Change Home page so it uses mock data

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,35 +4,11 @@ import Grid from "../../components/Grid/Grid";
 import Event from "../../components/Event/Event";
 
 import "./Home.scss";
-import ImageDesign from "../../assets/images/design.jpg";
-import ImageDevelopment from "../../assets/images/development.jpg";
-import ImageMarketing from "../../assets/images/marketing.jpg";
+import * as data from "../../utils/data"
 
 function Home() {
   //This events mock data will be replaced with an actual Axios/ApiSauce API call
-  const events = [
-    {
-      id: 1,
-      image: ImageDesign,
-      title: "UX/UI design workshop",
-      description:
-        "Learn how to solve big problems and test new ideas based on Google Sprint methodology..",
-    },
-    {
-      id: 2,
-      image: ImageDevelopment,
-      title: "Frontend best practices",
-      description:
-        "Best frontend practices for developers. Learn advanced CSS techniques and much more...",
-    },
-    {
-      id: 3,
-      image: ImageMarketing,
-      title: "Digital marketing workshop",
-      description:
-        "Learn how to use Google Ads and social networks to gain customers for your digital product...",
-    },
-  ];
+  const events = data.mockTopEvents;
 
   return (
     <>
@@ -40,9 +16,9 @@ function Home() {
       <Section title="Featured events">
         <Grid columns="3">
           {events.length ? (
-            events.map((eventItem, index) => (
+            events.map((eventItem) => (
               <Event
-                index={eventItem.id.toString()}
+                key={eventItem.id.toString()}
                 image={eventItem.image}
                 title={eventItem.title}
                 description={eventItem.description}
