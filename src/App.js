fd3371d90cddd400dde8b039271947e97636277e
aldin-alagic <aldin@dysordian.com>
2021-04-23 02:42:48 +0200

    Add Switch wrap to App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 
-import Event from "./pages/Event/Event";
 import Home from "./pages/Home/Home";
 
 //Static
@@ -14,16 +13,18 @@ import "./App.scss";
 
 class App extends Component {
   componentDidMount() {
-    window.scrollTo(0, 0)
+    window.scrollTo(0, 0);
   }
+  
   render() {
     return (
       <ScrollToTop>
         <Header />
         <Main>
-        <Route path="/event" component={Event} />
-          <Route path="/" component={Home} />
-          <Redirect to="/" />
+          <Switch>
+            <Route path="/" component={Home} />
+            <Redirect to="/" />
+          </Switch>
         </Main>
         <Footer />
       </ScrollToTop>
