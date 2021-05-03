6de8b90f7337b16197ca19751eeeb16d6489b229
aldin-alagic <aldin@dysordian.com>
2021-05-03 20:26:39 +0200

    Add HamburgerNav to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+
+import HamburgerNav from "../HamburgerNav/HamburgerNav";
 
 import {
   HeaderWrapper,
@@ -13,26 +15,25 @@ import {
 import LogoImage from "../../assets/images/logo.png";
 
 const Header = () => {
+  const [hamburgerNavOpened, setHamburgerNavOpened] = useState(false);
+
   return (
     <HeaderWrapper>
       <Inner>
         <LogoContainer to="/">
           <Logo src={LogoImage} alt="FOI logo" />
         </LogoContainer>
-        <Hamburger>
+        <Hamburger onClick={() => setHamburgerNavOpened(!hamburgerNavOpened)}>
           <HamburgerLine />
           <HamburgerLine />
           <HamburgerLine />
         </Hamburger>
         <Nav>
-          <NavItem to="/" >
-            Home
-          </NavItem>
-          <NavItem to="/events">
-            Events
-          </NavItem>
+          <NavItem to="/">Home</NavItem>
+          <NavItem to="/events">Events</NavItem>
         </Nav>
       </Inner>
+      <HamburgerNav opened={hamburgerNavOpened} />
     </HeaderWrapper>
   );
 };
