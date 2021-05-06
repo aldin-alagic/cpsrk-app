77ca16a57c988d711c34ae97ca23199ccab7f72f
aldin-alagic <aldin@dysordian.com>
2021-05-06 12:42:07 +0200

    Create empty Login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+import Section from "../../components/Section/Section";
+
+import { Title } from "../../lib/style/generalStyles";
+
+const Login = () => {
+  useEffect(() => {}, []);
+
+  return (
+    <>
+      <Title>Login</Title>
+      <Section withoutTopPadding>
+       
+      </Section>
+    </>
+  );
+};
+
+export default Login;
