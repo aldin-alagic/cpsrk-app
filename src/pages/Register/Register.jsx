49ae0e07524abd9724afde6f05d77d9d36a1e8d6
aldin-alagic <aldin@dysordian.com>
2021-05-06 12:42:16 +0200

    Create empty Register page

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+import Section from "../../components/Section/Section";
+
+import { Title } from "../../lib/style/generalStyles";
+
+const Register = () => {
+  useEffect(() => {}, []);
+
+  return (
+    <>
+      <Title>Register</Title>
+      <Section withoutTopPadding>
+       
+      </Section>
+    </>
+  );
+};
+
+export default Register;
