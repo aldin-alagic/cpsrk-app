90b8f550ca94f8adbad791a58dd22d2f92bfff7f
aldin-alagic <aldin@dysordian.com>
2021-05-06 12:42:32 +0200

    Add form components to generalStyles

--- src/lib/style/generalStyles.js
+++ src/lib/style/generalStyles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { Link } from "react-router-dom";
 
 import { colors, breakpoints, transitionEase } from "../style/theme";
@@ -43,7 +43,7 @@ export const Title = styled.h1`
   }
 `;
 
-export const Button = styled(Link)`
+export const ButtonDefault = css`
   display: block;
   text-decoration: none;
   width: 100%;
@@ -68,6 +68,30 @@ export const Button = styled(Link)`
   }
 `;
 
+export const Button = styled(Link)`
+  ${ButtonDefault}
+`;
+
+export const ButtonSubmit = styled.button`
+  ${ButtonDefault}
+`;
+
+export const ButtonTable = styled.button`
+  ${ButtonDefault}
+  width: 160px;
+  border-color: ${colors.black};
+  background: ${colors.white};
+  color: ${colors.black};
+  font-weight: 600;
+
+  &:hover {
+    border-color: ${colors.green}
+    background: ${colors.white}
+    color: ${colors.green};
+  }
+`;
+
+
 export const Main = styled.main`
   margin-top: 80px;
   flex: 1 0 auto;
@@ -77,4 +101,106 @@ export const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
+`;
+
+export const Form = styled.form`
+  margin: 0 auto; 
+
+  @media screen and (${breakpoints.mobileLarge}) {
+    width: 400px;
+
+    ${props => props.fullWidth && `
+      max-width: 100%;
+    `};
+  }
+`;
+
+export const FormRow = styled.div`
+  margin-bottom: 32px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+export const FormTwoColumns = styled.div`
+  margin-bottom: 32px;
+
+  @media screen and (${breakpoints.mobileLarge}) {
+    display: grid;
+    grid-template-columns: repeat (2, 1fr);
+    column-gap; 12px;
+  }
+`;
+
+export const CheckboxWrapper = styled.div`
+  margin-bottom: 32px;
+  display: flex;
+  align-items: center;
+`;
+
+export const InputLabel = styled.label`
+  display: block;
+  font-size: 14px;
+  font-weight: 600;
+  margin-bottom: 4px;
+
+  ${props => props.isCheckBox && `
+    margin-bottom: 0;
+  `};
+
+  @media screen and (${breakpoints.desktop}) {
+    font-size: 16px;
+  }
+`;
+
+export const InputText = styled.input`
+  border: 1px solid ${colors.lightGrey};
+  border-radius: 6px;
+  width: 100%;
+  line-height: 40px;
+  padding: 0 10px;
+  outline: none;
+  font-size: 14px;
+  font-family: 'Montserrat', sans-serif;
+
+  &:focus {
+    border-color: ${colors.yellow};
+  }
+
+  @media screen and (${breakpoints.desktop}) {
+    font-size: 16px;
+  }
+`;
+
+export const TextArea = styled.textarea`
+  border: 1px solid ${colors.lightGrey};
+  border-radius: 6px;
+  width: 100%;
+  line-height: 150%;
+  padding: 10px;
+  outline: none;
+  font-size: 14px;
+  font-family: 'Montserrat', sans-serif;
+  min-width: 100%;
+
+  &:focus {
+    border-color: ${colors.yellow};
+  }
+
+  @media screen and (${breakpoints.desktop}) {
+    font-size: 16px;
+  }
+`;
+
+export const InputCheckbox = styled.input`
+  margin-right: 12px;
+  width: 18px;
+  height: 18px;
+`;
+
+export const InputError = styled.p`
+  font-size: 14px;
+  colors: ${colors.red};
+  padding-top: 8px;
 `;
