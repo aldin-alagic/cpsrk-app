b9fa33201a60f86d8b2a59525c3b8e13bff0a61a
aldin-alagic <aldin@dysordian.com>
2021-05-10 03:22:30 +0200

    Create Table component

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,222 @@
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+import Modal from "../Modal/Modal";
+
+import {
+  TableWrapper,
+  TableHeader,
+  TableBody,
+  Th,
+  ThAction,
+  Tr,
+  Td,
+  TdAction,
+  RemoveButton,
+  TableEmpty,
+} from "./TableStyles";
+import {
+  ButtonTable,
+  Form,
+  FormRow,
+  FormTwoColumns,
+  InputLabel,
+  InputText,
+  TextArea,
+  InputError,
+  ButtonSubmit,
+} from "../../lib/style/generalStyles";
+
+const Table = () => {
+  const [events, setEvents] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      id: 1,
+      title: "",
+      description: "",
+      category: "",
+      date: "",
+      startTime: "",
+      endTime: "",
+      capacity: "",
+      company: "",
+    },
+
+    validationSchema: Yup.object({
+      title: Yup.string().required("Title is required"),
+      description: Yup.string().required("Description is required"),
+      category: Yup.string().required("Category is required"),
+      date: Yup.string().required("Date is required"),
+      startTime: Yup.string().required("Start time is required"),
+      endTime: Yup.string().required("End time is required"),
+      capacity: Yup.number().required("Capacity is required"),
+      company: Yup.string().required("Company name is required"),
+    }),
+
+    onSubmit: (values, {resetForm}) => {
+      setEvents([...events, values]);
+      setIsModalOpen(false);
+      resetForm({});
+    },
+  });
+
+  const handleDeleteEvent = (index) => {
+    const newEvents = [...events];
+    newEvents.splice(index, 1); 
+    setEvents(newEvents);
+  };
+
+  return (
+    <>
+      <ButtonTable onClick={() => setIsModalOpen(true)}>Add event</ButtonTable>
+      <Modal
+        title="Add event"
+        isVisible={isModalOpen}
+        handleClose={() => {
+          setIsModalOpen(false);
+        }}
+      >
+        <Form fullWidth onSubmit={formik.handleSubmit}>
+          <FormRow>
+            <InputLabel htmlFor="title">Title</InputLabel>
+            <InputText
+              id="title"
+              type="text"
+              {...formik.getFieldProps("title")}
+            />
+            {formik.touched.title && formik.errors.title && (
+              <InputError>{formik.errors.title}</InputError>
+            )}
+          </FormRow>
+          <FormRow>
+            <InputLabel htmlFor="description">Description</InputLabel>
+            <TextArea
+              id="description"
+              {...formik.getFieldProps("description")}
+            />
+            {formik.touched.description && formik.errors.description && (
+              <InputError>{formik.errors.description}</InputError>
+            )}
+          </FormRow>
+
+          <FormTwoColumns>
+            <FormRow>
+              <InputLabel htmlFor="category">Category</InputLabel>
+              <InputText
+                id="category"
+                type="text"
+                {...formik.getFieldProps("category")}
+              />
+              {formik.touched.category && formik.errors.category && (
+                <InputError>{formik.errors.category}</InputError>
+              )}
+            </FormRow>
+            <FormRow>
+              <InputLabel htmlFor="date">Date</InputLabel>
+              <InputText
+                id="date"
+                type="text"
+                {...formik.getFieldProps("date")}
+              />
+              {formik.touched.date && formik.errors.date && (
+                <InputError>{formik.errors.date}</InputError>
+              )}
+            </FormRow>
+          </FormTwoColumns>
+
+          <FormTwoColumns>
+            <FormRow>
+              <InputLabel htmlFor="startTime">Start time</InputLabel>
+              <InputText
+                id="startTime"
+                type="text"
+                {...formik.getFieldProps("startTime")}
+              />
+              {formik.touched.startTime && formik.errors.startTime && (
+                <InputError>{formik.errors.startTime}</InputError>
+              )}
+            </FormRow>
+            <FormRow>
+              <InputLabel htmlFor="endTime">End time</InputLabel>
+              <InputText
+                id="endTime"
+                type="text"
+                {...formik.getFieldProps("endTime")}
+              />
+              {formik.touched.endTime && formik.errors.endTime && (
+                <InputError>{formik.errors.endTime}</InputError>
+              )}
+            </FormRow>
+          </FormTwoColumns>
+
+          <FormTwoColumns>
+            <FormRow>
+              <InputLabel htmlFor="capacity">Capacity</InputLabel>
+              <InputText
+                id="capacity"
+                type="number"
+                {...formik.getFieldProps("capacity")}
+              />
+              {formik.touched.capacity && formik.errors.capacity && (
+                <InputError>{formik.errors.capacity}</InputError>
+              )}
+            </FormRow>
+            <FormRow>
+              <InputLabel htmlFor="company">Company</InputLabel>
+              <InputText
+                id="company"
+                type="text"
+                {...formik.getFieldProps("company")}
+              />
+              {formik.touched.company && formik.errors.company && (
+                <InputError>{formik.errors.company}</InputError>
+              )}
+            </FormRow>
+          </FormTwoColumns>
+          <FormRow>
+            <ButtonSubmit type="submit">Add event</ButtonSubmit>
+          </FormRow>
+        </Form>
+      </Modal>
+      {events.length ? (
+        <TableWrapper>
+          <TableHeader>
+            <Tr>
+              <Th>ID</Th>
+              <Th>Naslov</Th>
+              <Th>Datum</Th>
+              <Th>Vrijeme od</Th>
+              <Th>Vrijeme do</Th>
+              <Th>Kapacitet</Th>
+              <Th>Firma</Th>
+              <ThAction></ThAction>
+            </Tr>
+          </TableHeader>
+          <TableBody>
+            {events.map((event, index) => (
+              <Tr key={index}>
+                <Td>{event.id}</Td>
+                <Td>{event.title}</Td>
+                <Td>{event.date}</Td>
+                <Td>{event.startTime}</Td>
+                <Td>{event.endTime}</Td>
+                <Td>{event.capacity}</Td>
+                <Td>{event.company}</Td>
+                <TdAction>
+                  <RemoveButton onClick={() => handleDeleteEvent(index)} />
+                </TdAction>
+              </Tr>
+            ))}
+          </TableBody>
+        </TableWrapper>
+      ) : (
+        <TableEmpty>There are no events yet!</TableEmpty>
+      )}
+    </>
+  );
+};
+
+export default Table;
---
+++ src/components/Table/TableStyles.js
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+
+import { colors, breakpoints } from "../../lib/style/theme";
+
+import { MdDelete } from "react-icons/md";
+
+export const TableWrapper = styled.table`
+  table-layout: fixed;
+  width: 100%;
+  text-align: left;
+  border-collapse: collapse;
+  overflow: hidden;
+  box-shadow: 1px 0px 4px 6px rgb(0 0 0 / 12%), 0 1px 2px rgb(0 0 0 / 24%);
+  margin-top: 32px;
+  border-radius: 6px;
+`;
+
+export const TableHeader = styled.thead`
+  color: ${colors.white};
+  font-weight: 600;
+  background: ${colors.red};
+`;
+
+export const TableBody = styled.tbody``;
+
+export const Th = styled.th`
+  padding: 12px;
+  display: none;
+  
+  &:nth-child(-n + 2) {
+    display: table-cell !important;
+  }
+
+  @media screen and (${breakpoints.mobileLarge}) {
+    &:nth-child(3), &:nth-child(7) {
+      display: table-cell !important;
+    }
+  }
+
+  @media screen and (${breakpoints.tablet}) {
+    display: table-cell !important;
+  }
+`;
+
+export const ThAction = styled.th``;
+
+export const Tr = styled.tr`
+  &:nth-of-type(even) {
+    background: ${colors.lighterGrey};
+  }
+`;
+
+export const Td = styled.td`
+  padding: 12px;
+  display: none;
+  
+  &:nth-child(-n + 2) {
+    display: table-cell !important;
+  }
+
+  @media screen and (${breakpoints.mobileLarge}) {
+    &:nth-child(3), &:nth-child(7) {
+      display: table-cell !important;
+    }
+  }
+
+  @media screen and (${breakpoints.tablet}) {
+    display: table-cell !important;
+  }
+`;
+
+export const TdAction = styled.td`
+  text-align: center;
+  padding: 12px;
+`;
+
+export const RemoveButton = styled(MdDelete)`
+  &:hover {
+    cursor: pointer;
+    color: ${colors.red};
+  }
+`;
+
+export const TableEmpty = styled.p`
+  font-size: 16px;
+  font-weight: 600;
+  text-align: center;
+  padding: 20px 0;
+`;
+
+
