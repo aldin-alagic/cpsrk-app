70fc3989b8df0520409d58207f9c5dac747b1396
aldin-alagic <aldin@dysordian.com>
2021-05-11 15:54:43 +0200

    Add companies and event categories mock data

---
+++ src/lib/mock/companies.js
@@ -0,0 +1,9 @@
+const companiesMock = [
+    {value: "Speck", label: "Speck"},
+    {value: "Five", label: "Five"},
+    {value: "Bornfight", label: "Bornfight"},
+    {value: "Agency 04", label: "Agency 04"},
+    {value: "Infinum", label: "Infinum"},
+];
+
+export default companiesMock;
---
+++ src/lib/mock/eventCategories.js
@@ -0,0 +1,9 @@
+const categoriesMock = [
+    {value: "marketing", label: "#marketing"},
+    {value: "design", label: "#design"},
+    {value: "frontend", label: "#frontend"},
+    {value: "backend", label: "#backend"},
+
+];
+
+export default categoriesMock;
